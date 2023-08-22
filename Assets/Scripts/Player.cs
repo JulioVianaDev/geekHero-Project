15bf2e900c316aa396d72b7df06e36a16f71c081
juliovianadev <juliovianadev@gmail.com>
2023-08-21 23:08:01 -0300

    aula 2 antes do atack finalizado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-   public float moveSpeed;
+    public float moveSpeed;
     public Rigidbody2D rig2D;
     public float moveX;
     public float moveY;
 
-    
+    bool isMoving;
+    Animator anim;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,9 +23,23 @@ public class Player : MonoBehaviour
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
         Movement();
+        Animation();
     }
 
     void Movement(){
         rig2D.MovePosition(transform.position + new Vector3(moveX, moveY, 0) * Time.deltaTime * moveSpeed);
+    }
+    void Animation(){
+        if(moveX == 0 && moveY == 0)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("Horizontal", moveX);
+        anim.SetFloat("Vertical", moveY);
     }
 }
